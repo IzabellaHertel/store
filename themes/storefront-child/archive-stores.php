fee78d48e8f7e1e60bd9a2c296f1fbc819beddc4
izabella <izabellahertel@live.se>
2018-01-04 13:12:03 +0100

    our stores styling

--- themes/storefront-child/archive-stores.php
+++ themes/storefront-child/archive-stores.php
@@ -2,10 +2,14 @@
 
     if (have_posts()) {
         while (have_posts()) { the_post(); ?>
-            <h2><?= get_the_title(); ?></h2>
-            <?php the_content();
-            show_map();
-        }
+            <div class="store">
+                <div class="content">
+                    <h2><?php the_title(); ?></h2>
+                    <?php the_content(); ?>
+                </div>
+                <?php show_map(); ?>
+            </div>
+        <?php }
     }
 
 get_footer();
