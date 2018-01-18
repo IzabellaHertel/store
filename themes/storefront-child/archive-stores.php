426fe0c3e08fcb9c007a1e7dcc2d7eb0797f51c9
izabella <izabellahertel@live.se>
2018-01-18 14:43:55 +0100

    added single store

--- themes/storefront-child/archive-stores.php
+++ themes/storefront-child/archive-stores.php
@@ -4,7 +4,7 @@
         while (have_posts()) { the_post(); ?>
             <div class="store">
                 <div class="content">
-                    <h2><?php the_title(); ?></h2>
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                     <?php the_content(); ?>
                 </div>
                 <?php show_map(); ?>
