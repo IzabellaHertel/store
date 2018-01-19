6ac2b59fe44260ca9b4874198f2f6bf9f8566c28
izabella <izabellahertel@live.se>
2018-01-19 13:23:21 +0100

    changed to endwhile instead of {

--- themes/storefront-child/archive-stores.php
+++ themes/storefront-child/archive-stores.php
@@ -1,7 +1,7 @@
 <?php get_header();
 
-    if (have_posts()) {
-        while (have_posts()) { the_post(); ?>
+    if (have_posts()) :
+        while (have_posts()) : the_post(); ?>
             <div class="store">
                 <div class="content">
                     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
@@ -11,7 +11,7 @@
                     <?php show_map(); ?>
                 </div>
             </div>
-        <?php }
-    }
+        <?php endwhile;
+    endif;
 
 get_footer();
